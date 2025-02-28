b5ccfb9531cc86864fe559eb9b6b47667129ed6f
skvillamin <135067148+skvillamin@users.noreply.github.com>
2025-02-27 16:04:55 -0800

    fixed avg file length

--- meta/main.js
+++ meta/main.js
@@ -180,7 +180,7 @@ function displayStats() {
       (v) => d3.max(v, (d) => d.line),
       (d) => d.file
     );
-    const averageFileLength = d3.mean(fileLengths, (d) => d[1]);
+    const averageFileLength = Math.round(d3.mean(fileLengths, (d) => d[1]));    
     container = dl.append('div').attr('class', 'stat-block');
     container.append('dt').text('Average File Length');
     container.append('dd').text(averageFileLength);
